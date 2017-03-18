2e5a229adce022eef5a98fcb5a30f86d3a609b53
rpchen <g1222888@gmail.com>
2017-03-18 20:40:49 +0800

    remove a field in user account

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,18 +6,10 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     orders = OrderSerializer(many=True)
-    group_id = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'deposit', 'orders', 'group_id') # put Chinese name in first_name
-
-    def get_group_id(self, obj):
-        group = obj.groups.first()
-        if not group:
-            return None
-        else:
-            return group.pk
+        fields = ('username', 'first_name', 'deposit', 'orders') # put Chinese name in first_name
 
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
